ee8ff6857f63180d50e5476c52e7c35902050e7c
Kayan Galvani <kayan.galvani30@gmail.com>
2023-03-06 21:44:11 -0300

    py

--- 1/atividade3.py
+++ 1/atividade3.py
@@ -3,5 +3,7 @@ numero2 = input("Digite outro numero: ")
 
 if numero1 > numero2:  
     print ("O primeiro numero é maior")
-else:
+elif numero1 < numero2:
     print ("O segundo numero é maior")
+else:
+    print ("Os numeros são iguais")
